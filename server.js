c5fc2a557d438d1fc4fa9b35b57577d16300d720
Your Name <your.email@example.com>
2025-06-15 00:39:51 +0530

    again

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cors({
     'http://localhost:3001',
     'http://localhost:5173',
     'http://localhost:5175',
-    'https://techsolution-gamma.vercel.app',
+    'https://techsolution-sr1g.vercel.app',
     'https://techsolutionback.onrender.com'
   ],
   credentials: true,
